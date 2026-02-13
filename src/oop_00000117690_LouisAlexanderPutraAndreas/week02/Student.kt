b9ee0626e0f3e7a6b9a62bb73521c409e7753bab
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-02-13 15:21:52 +0700

    week02: add warning validation in init block

--- src/oop_00000117690_LouisAlexanderPutraAndreas/week02/Student.kt
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week02/Student.kt
@@ -1,9 +1,13 @@
 package oop_00000117690_LouisAlexanderPutraAndreas.week02
 
-class Student (
-    val name: String,
-    val nim: String,
-    var major: String
-){
-    // Kosong
+class Student (val name: String, val nim: String, var major: String){
+    init {
+        // Validasi cek panjang NIM
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
+        }
+    }
 }
