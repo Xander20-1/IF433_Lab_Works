534ef9b6fc09d67ef0b83729f1506fb844136f66
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-02-20 15:12:45 +0700

    week03: cleanup code

--- src/oop_00000117690_LouisAlexanderPutraAndreas/week03/Main.kt
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week03/Main.kt
@@ -3,15 +3,9 @@ package oop_00000117690_LouisAlexanderPutraAndreas.week03
 fun main() {
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
-
-    // 2. Test Encapsulation
     e.increasePerformance()
-    e.performanceRating = 5
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
