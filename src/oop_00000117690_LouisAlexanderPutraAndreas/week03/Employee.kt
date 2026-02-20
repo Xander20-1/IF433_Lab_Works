1455a08d086e1e32707625933b0bfc850e28e380
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-02-20 14:55:56 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000117690_LouisAlexanderPutraAndreas/week03/Employee.kt
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000117690_LouisAlexanderPutraAndreas.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            // Kode di bawah ini salah karena memanggil setter di dalam setter
+            this.salary = value
+        }
 }
---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week03
+
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
