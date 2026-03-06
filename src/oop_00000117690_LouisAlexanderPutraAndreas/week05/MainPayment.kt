205e58444c5a976cd00127be073195dcf36f9dfb
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-03-06 15:32:37 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week05/MainPayment.kt
@@ -0,0 +1,13 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week05
+
+fun main() {
+    val Ewallet1 = Ewallet(accountName= "Loky",balance= 50000.0)
+    val creditCard1 = CreditCard(accountName= "Shiro", limit = 100000.0)
+
+    val tipePembayaran: List<PaymentMethod> = listOf(Ewallet1, creditCard1)
+
+    for (pembayaran in tipePembayaran) {
+        pembayaran.processPayment(amount= 75000.0)
+        println("-----------------------------")
+    }
+}
