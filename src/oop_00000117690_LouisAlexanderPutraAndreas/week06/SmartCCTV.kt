40c44bb165eaaefa162336899872a36895dcbf3b
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-03-13 15:49:52 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week06/SmartCCTV.kt
@@ -0,0 +1,20 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week06
+
+class SmartCCTV(override val id : String, override var name : String) : SmartDevice, Switchable, Recordable
+{
+    override fun startRecord() {
+        println("SmartCCTV $name mulai merekam.")
+    }
+
+    override fun stopRecord() {
+        println("SmartCCTV $name berhenti merekam.")
+    }
+    override fun turnOn{
+        println("SmartCCTV $name turned on")
+        startRecord()
+    }
+    override fun turnOff(){
+        println("SmartCCTV $name turned off")
+        stopRecord()
+    }
+}
