7f8548869b9daa5d6f1d851916101c7ea0411b84
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-03-13 14:52:54 +0700

    week06: fix Clickable interface using abstract property

--- src/oop_00000117690_LouisAlexanderPutraAndreas/week06/Clickable.kt
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week06/Clickable.kt
@@ -1,9 +1,6 @@
 package oop_00000117690_LouisAlexanderPutraAndreas.week06
 
 interface Clickable {
-    // Error: karena di interface tidak bisa menyimpan backing field
-    val name: String = "Tombol Rahasia"
-
-    // Function without body
+    val name: String // Abstract property
     fun click()
 }
