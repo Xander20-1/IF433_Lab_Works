8372129e619bba34ed7d6cabcfe3e19a4393383e
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-03-13 16:02:18 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week06/SmartHomeHub.kt
@@ -0,0 +1,20 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week06
+
+class SmartHomeHub(){
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice){
+        devices.add(device)
+        println("Berhasil menambahkan ${device.name}")
+    }
+
+    fun turnOffAllSwitches(){
+        println("\nMematikan semua perangkat Switchable")
+        for (device in devices){
+            if (device is Switchable){
+                device.turnOff()
+            }
+        }
+    }
+
+}
