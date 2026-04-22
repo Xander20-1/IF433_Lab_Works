e2db785b896d6e82f855e94bbd9736b4eb04d861
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-04-22 21:54:07 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week08;
+
+public class JavaPaymentService{
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
