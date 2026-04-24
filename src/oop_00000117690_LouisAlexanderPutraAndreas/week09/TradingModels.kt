8ea9adcf5837483ccf1ddd97f5064d734fde9210
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-04-24 09:26:06 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week09/TradingModels.kt
@@ -0,0 +1,7 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week09
+
+data class TradeLog(val pair: String,
+                    val position: String,
+                    val leverage: Int,
+                    val roe: Double,
+                    val status: String)
