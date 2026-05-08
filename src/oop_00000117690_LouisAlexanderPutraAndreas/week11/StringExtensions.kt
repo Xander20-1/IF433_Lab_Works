de3d1e09ab5b9080d2ecf153a4704447654e9bac
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-08 14:48:55 +0700

    week11: create string extension functions

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week11/StringExtensions.kt
@@ -0,0 +1,11 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week11
+
+// Extension function dengan receiver 'this'
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+// Extension function dengan parameter
+fun String.repeatTimes(n: Int) : String {
+    return this.repeat(n)
+}
