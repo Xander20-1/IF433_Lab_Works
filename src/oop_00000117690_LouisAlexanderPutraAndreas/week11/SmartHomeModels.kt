79851422ed0472f468853553ca7717cd2c643e34
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-08 15:18:33 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week11/SmartHomeModels.kt
@@ -0,0 +1,6 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week11
+
+data class SmartDevice(var name: String,
+                       var category: String,
+                       var isOnline: Boolean = false,
+                       var powerLoad: Int = 0)
