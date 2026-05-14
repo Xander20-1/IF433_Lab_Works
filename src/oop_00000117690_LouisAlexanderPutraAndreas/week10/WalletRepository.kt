82be4976ee5384bd9b25ac0efb601fbbccd951c6
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-14 18:10:18 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000117690_LouisAlexanderPutraAndreas/week10/WalletRepository.kt
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week10/WalletRepository.kt
@@ -11,4 +11,14 @@ class WalletRepository<T> {
         return items
     }
 
+    fun findByName(searchName: String): T? {
+        return items.find { item ->
+            // Mengecek apakah item tersebut adalah tipe data Coin
+            if (item is Coin) {
+                item.name.equals(searchName, ignoreCase = true)
+            } else {
+                false
+            }
+        }
+    }
 }
