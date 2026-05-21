20592781a23c796ca76eadf2c6ccfe384fd271c0
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-21 19:16:08 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week12/SmartFeeder.kt
@@ -0,0 +1,9 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int{
+    require(requestedGram > 0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    return availableGram - requestedGram
+}
+
