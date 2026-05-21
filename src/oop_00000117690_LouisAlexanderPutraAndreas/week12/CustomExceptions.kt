0cb8d9d960cd3bdc39343060a158c34ac44bdc12
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-21 18:43:12 +0700

    week12: create InsufficientFunds exception

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week12/CustomExceptions.kt
@@ -0,0 +1,7 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week12
+
+// Custom exception dengan membawa data tambahan
+class InsufficientFundsException(
+    val amount: Double,
+    val balance: Double
+) : Exception("Attempted $amount, balance: $balance")
