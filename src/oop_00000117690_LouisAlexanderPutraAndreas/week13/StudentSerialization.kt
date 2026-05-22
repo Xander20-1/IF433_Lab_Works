ee55db653cffaf7fa580517e53807a34ccad134e
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-22 14:49:32 +0700

    week13: create Student model

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week13/StudentSerialization.kt
@@ -0,0 +1,10 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
