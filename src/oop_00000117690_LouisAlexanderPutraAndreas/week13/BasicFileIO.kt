ba18a515710278709ee5e1ab5e3dcfff09de02f3
LouisAlexanderAndreas <louwisa18@gmail.com>
2026-05-22 13:25:46 +0700

    week13: implement basic writeText function

---
+++ src/oop_00000117690_LouisAlexanderPutraAndreas/week13/BasicFileIO.kt
@@ -0,0 +1,10 @@
+package oop_00000117690_LouisAlexanderPutraAndreas.week13
+
+import java.io.File
+
+fun main() {
+    println("=== TEST WRITE TEXT ===")
+    val file = File("notes.txt")
+    file.writeText("Line 1: Insialisasi sistem.\n")
+    println("File berhasil dibuat dan ditulis.")
+}
